9900da30d8f8b1f3501312e6bae044fa5f7d520e
Hurt78 <hp78@mail.ru>
2017-04-03 15:31:26 +0300

    pravka

--- js/js.js
+++ js/js.js
@@ -2,3 +2,4 @@
  document.onselectstart = noselect; // запрет на выделение элементов страницы //
  document.oncontextmenu = noselect; // запрет на выведение контекстного меню //
  function noselect() {return false;}
+
